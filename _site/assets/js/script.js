68e84f34a3f9533e69c79507a2ff21b8d32c7448
bib <biapolita@gmail.com>
2021-06-10 20:50:02 -0300

    the creating new moment form

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -2,7 +2,7 @@ var embedUrl = "";
 var player = null; 
 var playerLabel = null; 
 
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
     player = document.getElementById("embed");
     playerLabel = document.getElementById("modalEmbedLabel"); 
 });
@@ -18,4 +18,18 @@ function setEmbed(title,url) {
 function reload(){
     if (player == null) player = document.getElementById("embed");
     player.setAttribute("src",embedUrl);
+}
+
+function openModalWithCode(){
+    var url = document.forms["newMomentForm"]["videoUrl"].value;
+    var start = document.forms["newMomentForm"]["start"].value;
+    var end = document.forms["newMomentForm"]["end"].value;
+    var text = document.forms["newMomentForm"]["momentText"].value;
+    var id = url.split('/').pop();
+    var t = [...start.split(":"),...end.split(":")];
+    for (i = 0; i < 4; i++) { 
+        if (!t[i] || isNaN(t[i])) t[i] = 0;
+    }
+    console.log(url, start, end, text, id, t)
+    return false;
 }
