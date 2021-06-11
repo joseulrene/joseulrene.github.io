166a67fd24190a0eb88e753fe438c4d7cf5d98d5
bib <biapolita@gmail.com>
2021-06-10 21:40:39 -0300

    new moment w modal finalized

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,15 +21,50 @@ function reload(){
 }
 
 function openModalWithCode(){
+    var date = document.forms["newMomentForm"]["momentDate"].value;
     var url = document.forms["newMomentForm"]["videoUrl"].value;
-    var start = document.forms["newMomentForm"]["start"].value;
-    var end = document.forms["newMomentForm"]["end"].value;
+    var start = document.forms["newMomentForm"]["start"].value.split(":");
+    var end = document.forms["newMomentForm"]["end"].value.split(":");
     var text = document.forms["newMomentForm"]["momentText"].value;
     var id = url.split('/').pop();
-    var t = [...start.split(":"),...end.split(":")];
+    var hasStart = document.forms["newMomentForm"]["urlHasStart"].checked;
+    if (!start[1]) start[1] = 0;
+    if (!end[1]) end[1] = 0;
+    var t = [start[0],start[1],end[0],end[1]];
     for (i = 0; i < 4; i++) { 
         if (!t[i] || isNaN(t[i])) t[i] = 0;
     }
-    console.log(url, start, end, text, id, t)
+    console.log(url, start, end, text, id, t, hasStart)
+    
+    var startsecs = t[0]*60+t[1];
+    var endsecs = t[2]*60+t[3];
+
+    // var vliveurl = `https://vlive.tv/embed/${i.id}?&begin=${startsecs}&end=${endsecs}`;
+    // var yturl = `https://youtube.com/embed/${i.id}?start=${start}&end=${end}`;
+    var appendStartVlive = `?&begin=${startsecs}`;
+    var appendStartYt = `?start=${startsecs}`;
+    var appendEnd = `&end=${endsecs}`;
+    if (!hasStart) 
+        if (url.includes("vlive")) 
+            url += appendStartVlive;
+        else
+            url += appendStartYt;
+    url += appendEnd;
+    var i = {
+        id: id,
+        start: startsecs == 0 ? endsecs : startsecs,
+        url: url,
+        date : date,
+        text: text,
+        // bonus: i.bonus ? i.bonus : ""
+    }
+    var result = ""
+    for (var p in i){
+        var newLine = `  ${p}: ${i[p]}\n`;
+        if (p == "id") newLine = newLine.replace("  ","- ");
+        result += newLine;
+    }
+    console.log(result)
+    document.getElementById("newMoment").value = result;
     return false;
 }
