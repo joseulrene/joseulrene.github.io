bd5b6ce082d78904d43eb97cab00c87835761d88
joseulrene <biapolita@gmail.com>
2023-02-14 22:05:42 -0300

    Screen switching

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1676344310|26379180573";
+const CACHE_VERSION = "1676422807|6101042109";
 const CACHE_PREFIX = "TamagotchiRV-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
